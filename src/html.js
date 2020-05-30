c969d0cd6e9f5640d10e013735b63787179bc75e
Jay S <allesyaj@gmail.com>
2020-05-30 13:28:53 -0400

    fix: preload fonts

--- src/html.js
+++ src/html.js
@@ -7,11 +7,9 @@ export default function HTML(props) {
       <head>
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1, shrink-to-fit=no"
-        />
-        <link href="https://fonts.googleapis.com/css2?family=Bellota+Text:ital,wght@0,300;1,700&display=swap" rel="stylesheet"></link>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <link rel="preload" href="https://fonts.googleapis.com/css2?family=Bellota+Text:ital,wght@0,300;1,700&display=swap" as="style" onload="this.onload=null;this.rel='stylesheet'"></link>
+        <noscript><link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Bellota+Text:ital,wght@0,300;1,700&display=swap"></link></noscript>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
